587bf87f178b258de7c2aee4dbbe566d67727581
0xAkira <eweeda172@gmail.com>
2024-11-29 15:43:19 +0100

    test

---
+++ app/(storefront)/page.tsx
@@ -0,0 +1,16 @@
+import { CategoriesSelection } from "../components/storefront/CategorySelection";
+import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
+import { LandingHeader } from "../components/storefront/LandingHeader";
+import { Hero } from "../components/storefront/Hero";
+import { Navbar } from "../components/storefront/Navbar";
+
+export default function IndexPage() {
+  return (
+    <div>
+      {/* <Hero /> */}
+      {/* <LandingHeader /> */}
+      <CategoriesSelection />
+      <FeaturedProducts />
+    </div>
+  );
+}
---
+++ app/components/storefront/LandingHeader.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import all from "@/public/all.jpeg";
+import men from "@/public/men.jpeg";
+import women from "@/public/women.jpeg";
+
+export function LandingHeader() {
+  return (
+    <div className="py-24 sm:py-32 w-full bg-red-500 flex flex-row">
+      <div className="w-1/2 flex flex-col">
+        <h1>hi</h1>
+      </div>
+      <div className="w-1/2 flex flex-col">
+        <h1>hi</h1>
+        <h1>hi</h1>
+      </div>
+    </div>
+  );
+}
