c5e8e04deba0cc6acba3ef88932d4f3d71ed4242
0xAkira <eweeda172@gmail.com>
2025-03-18 10:20:47 +0100

    test 1

--- app/[lang]/(storefront)/bag/page.tsx
+++ app/[lang]/(storefront)/bag/page.tsx
@@ -1,37 +1,43 @@
-import { checkOut, delItem } from "@/app/actions"
-import { ChceckoutButton, DeleteItem } from "@/app/components/SubmitButtons"
-import type { Cart } from "@/app/lib/interfaces"
-import { redis } from "@/app/lib/redis"
-import { Button } from "@/components/ui/button"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { ShoppingBag } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import { unstable_noStore as noStore } from "next/cache"
-import { getDictionary } from "@/app/[lang]/dictionaries"
-import { redirect } from "next/navigation"
+import { checkOut, delItem } from "@/app/actions";
+import { ChceckoutButton, DeleteItem } from "@/app/components/SubmitButtons";
+import type { Cart } from "@/app/lib/interfaces";
+import { redis } from "@/app/lib/redis";
+import { Button } from "@/components/ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ShoppingBag } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { unstable_noStore as noStore } from "next/cache";
+import { getDictionary } from "@/app/[lang]/dictionaries";
+import { redirect } from "next/navigation";
 
-export default async function BagRoute({ params }: { params: { lang: "en" | "ar" } }) {
-  noStore()
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-  const dict = await getDictionary(params.lang)
-  const isRtl = params.lang === "ar"
+export default async function BagRoute({
+  params,
+}: {
+  params: { lang: "en" | "ar" };
+}) {
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const dict = await getDictionary(params.lang);
+  const isRtl = params.lang === "ar";
 
   if (!user) {
-    redirect(`/${params.lang}`)
+    redirect(`/${params.lang}`);
   }
 
-  const cart: Cart | null = await redis.get(`cart-${user.id}`)
+  const cart: Cart | null = await redis.get(`cart-${user.id}`);
 
-  let totalPrice = 0
+  let totalPrice = 0;
 
   cart?.items.forEach((item) => {
-    totalPrice += item.price * item.quantity
-  })
+    totalPrice += item.price * item.quantity;
+  });
 
   return (
-    <div className={`max-w-2xl mx-auto mt-10 min-h-[55vh] ${isRtl ? "rtl" : ""}`}>
+    <div
+      className={`max-w-2xl mx-auto mt-10 min-h-[55vh] ${isRtl ? "rtl" : ""}`}
+    >
       {!cart || !cart.items ? (
         <div className="flex min-h-[400px] flex-col items-center justify-center rounded-lg border border-dashed p-8 text-center mt-20">
           <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
@@ -50,7 +56,10 @@ export default async function BagRoute({ params }: { params: { lang: "en" | "ar"
       ) : (
         <div className="flex flex-col gap-y-10">
           {cart?.items.map((item) => (
-            <div key={item.id} className={`flex ${isRtl ? "flex-row-reverse" : ""}`}>
+            <div
+              key={item.id}
+              className={`flex ${isRtl ? "flex-row-reverse" : ""}`}
+            >
               <div className="w-24 h-24 sm:w-32 sm:h-32 relative">
                 <Image
                   className="rounded-md object-cover"
@@ -59,39 +68,65 @@ export default async function BagRoute({ params }: { params: { lang: "en" | "ar"
                   alt="Product image"
                 />
               </div>
-              <div className={`${isRtl ? "mr-5" : "ml-5"} flex justify-between w-full font-medium`}>
+              <div
+                className={`${
+                  isRtl ? "mr-5" : "ml-5"
+                } flex justify-between w-full font-medium`}
+              >
                 <p>{item.name}</p>
-                <div className={`flex flex-col h-full justify-between ${isRtl ? "items-start" : "items-end"}`}>
-                  <div className={`flex items-center gap-x-2 ${isRtl ? "flex-row-reverse" : ""}`}>
+                <div
+                  className={`flex flex-col h-full justify-between ${
+                    isRtl ? "items-start" : "items-end"
+                  }`}
+                >
+                  <div
+                    className={`flex items-center gap-x-2 ${
+                      isRtl ? "flex-row-reverse" : ""
+                    }`}
+                  >
                     <p>{item.quantity} x</p>
-                    <p>{isRtl ? `${item.price} ${dict.product.price}` : `${dict.product.price}${item.price}`}</p>
+                    <p>
+                      {isRtl
+                        ? `${item.price} ${dict.product.price}`
+                        : `${dict.product.price}${item.price}`}
+                    </p>
                   </div>
 
-                  <form action={delItem} className={isRtl ? "text-start" : "text-end"}>
+                  <form
+                    action={delItem}
+                    className={isRtl ? "text-start" : "text-end"}
+                  >
                     <input type="hidden" name="productId" value={item.id} />
-                    <DeleteItem lang={params.lang} />
+                    <DeleteItem />
                   </form>
                 </div>
               </div>
             </div>
           ))}
           <div className="mt-10">
-            <div className={`flex items-center justify-between font-medium ${isRtl ? "flex-row-reverse" : ""}`}>
+            <div
+              className={`flex items-center justify-between font-medium ${
+                isRtl ? "flex-row-reverse" : ""
+              }`}
+            >
               <p>{dict.cart.subtotal}</p>
               <p>
                 {isRtl
-                  ? `${new Intl.NumberFormat("en-US").format(totalPrice)} ${dict.product.price}`
-                  : `${dict.product.price}${new Intl.NumberFormat("en-US").format(totalPrice)}`}
+                  ? `${new Intl.NumberFormat("en-US").format(totalPrice)} ${
+                      dict.product.price
+                    }`
+                  : `${dict.product.price}${new Intl.NumberFormat(
+                      "en-US"
+                    ).format(totalPrice)}`}
               </p>
             </div>
 
             <form action={checkOut}>
-              <ChceckoutButton lang={params.lang} />
+              <ChceckoutButton />
             </form>
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
-
